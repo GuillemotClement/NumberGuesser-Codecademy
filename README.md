bef0320e9ac6cf6c043b3a0db736e09864d2a585
GuillemotClement <guillemot.clement@protonmail.com>
2023-11-07 11:12:01 +0100

    Ajout de la fonction compareGuesses()

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Dans ce projet, vous écrirez des fonctions JavaScript pour alimenter un petit j
 Le travail s'effectue dans le fichier `script.js`.
 Il consiste à l'écriture de 4 fonctions :
 - `generateTarget()`: renvoie un nombre entier entre 0 et 9
-- 
+- `compareGuesses()`: permet de déterminer qui est le plus proche du bon nombre
 ## ![Amelioration Logo](/ressource/ameliorationLogo.png) Amélioration future
 
 
--- script.js
+++ script.js
@@ -6,4 +6,17 @@ let currentRoundNumber = 1;
 //Fonction qui vient générer un nombre aléatoire de 0 à 9
 const generateTarget = () => Math.floor(Math.random() * 10);
 
+//Fonction qui détermine le plus proche du numéro à deviner
+const compareGuesses = (nbUser, nbComputer, nbSecret) => {
+    let difUser = Math.abs(nbUser - nbSecret)
+    let difComputer = Math.abs(nbComputer - nbSecret);
+    if(difUser < difComputer){
+        return true;
+    }else{
+        return false;
+    };
+};
+
+
+
 
